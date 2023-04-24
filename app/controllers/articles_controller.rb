16dc8557454d5f825eaccbf11b8d4f6f4b1e23c8
Mordechai <mordwein77@gmail.com>
2023-04-24 19:20:27 -0400

    commit tree 4830a057928a569696cd8e41bc03d10b2e660963

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,10 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    articles = Article.where(is_member_only: true)
+    # .includes(:user).order(created_at: :desc)
+    render json: articles
+    # , each_serializer: ArticleListSerializer
   end
 
   def show
